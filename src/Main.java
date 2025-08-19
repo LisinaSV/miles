720a3120f4b8767f784a77dddce20f70a867b138
Lisina <lisina.pro@gmail.com>
2025-08-19 21:43:37 +0300

    main

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int amount = 13676;
         int oneMiles = 20;
-
-        int miles;
-        if (amount >= 20) {
-            miles = 1;
-
-        } else {
-            miles = 0;
-        }
-
         int balace = amount / oneMiles;
 
         System.out.println("Накопленные мили =" + balace);
